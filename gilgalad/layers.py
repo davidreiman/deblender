0f197dee802a80d11dee9f17957c44d1c953d217
David <dreiman@ucsc.edu>
2019-01-23 09:56:21 -0800

    General updates and improvements

--- gilgalad/layers.py
+++ gilgalad/layers.py
@@ -27,7 +27,7 @@ def res_block_1d(x, kernel_size, activation, batch_norm=True):
         kernel_size=kernel_size,
         strides=1,
         padding='same'
-        )
+    )
 
     if batch_norm:
         y = ly.batch_normalization(y)
@@ -40,7 +40,7 @@ def res_block_1d(x, kernel_size, activation, batch_norm=True):
         kernel_size=kernel_size,
         strides=1,
         padding='same'
-        )
+    )
 
     if batch_norm:
         y = ly.batch_normalization(y)
@@ -61,7 +61,7 @@ def res_block_2d(x, kernel_size, activation, batch_norm=True):
         kernel_size=kernel_size,
         strides=1,
         padding='same'
-        )
+    )
 
     if batch_norm:
         y = ly.batch_normalization(y)
@@ -74,7 +74,7 @@ def res_block_2d(x, kernel_size, activation, batch_norm=True):
         kernel_size=kernel_size,
         strides=1,
         padding='same'
-        )
+    )
 
     if batch_norm:
         y = ly.batch_normalization(y)
@@ -82,19 +82,20 @@ def res_block_2d(x, kernel_size, activation, batch_norm=True):
     return tf.add(x, y)
 
 
-def subpixel_convolution(x, upscale_ratio, activation, kernel_size=3, stride=1):
+def subpixel_conv(x, upscale_ratio, activation, kernel_size=3):
 
     activation = activation.lower()
     assert len(x.shape) == 4, "Input tensor must be 4-dimensional."
+    assert isinstance(upscale_ratio, int), "Upscale ratio must be integer."
 
     n_filters = int(x.shape[3])
 
     y = ly.conv2d(
-    inputs=x,
-    filters=n_filters*upscale_ratio**2,
-    kernel_size=kernel_size,
-    strides=stride,
-    padding='same'
+        inputs=x,
+        filters=n_filters*upscale_ratio**2,
+        kernel_size=kernel_size,
+        strides=1,
+        padding='same'
     )
 
     y = tf.depth_to_space(y, block_size=upscale_ratio)
@@ -110,11 +111,11 @@ def conv_block_1d(x, kernel_size, filters, stride, activation,
     activation = activation.lower()
 
     y = ly.conv1d(
-    inputs=x,
-    filters=filters,
-    kernel_size=kernel_size,
-    strides=stride,
-    padding='same'
+        inputs=x,
+        filters=filters,
+        kernel_size=kernel_size,
+        strides=stride,
+        padding='same'
     )
 
     y = nonlinear[activation](y)
@@ -131,11 +132,11 @@ def conv_block_2d(x, kernel_size, filters, stride, activation,
     activation = activation.lower()
 
     y = ly.conv2d(
-    inputs=x,
-    filters=filters,
-    kernel_size=kernel_size,
-    strides=stride,
-    padding='same'
+        inputs=x,
+        filters=filters,
+        kernel_size=kernel_size,
+        strides=stride,
+        padding='same'
     )
 
     y = nonlinear[activation](y)
@@ -151,11 +152,11 @@ def conv_2d(x, kernel_size, filters, stride, activation):
     activation = activation.lower()
 
     y = ly.conv2d(
-    inputs=x,
-    filters=filters,
-    kernel_size=kernel_size,
-    strides=stride,
-    padding='same'
+        inputs=x,
+        filters=filters,
+        kernel_size=kernel_size,
+        strides=stride,
+        padding='same'
     )
 
     y = nonlinear[activation](y)
--- main.py
+++ main.py
@@ -8,20 +8,40 @@ class ResNet:
         self.name = name
         self.num_blocks = num_blocks
 
-    def __call__(self, x):
-        x = gg.layers.conv_2d(x, kernel_size=3, filters=64, stride=1, activation='prelu')
+    def __call__(self, x, reuse=False):
+        with tf.variable_scope(self.name) as vs:
+            if reuse:
+                vs.reuse_variables()
+
+        x = gg.layers.conv_2d(
+            x,
+            kernel_size=3,
+            filters=64,
+            stride=1,
+            activation='prelu')
 
         x_ = tf.identity(x)
 
         for i in range(self.num_blocks):
-            x = gg.layers.res_block_2d(x, kernel_size=3, activation='prelu')
+            x = gg.layers.res_block_2d(
+                x,
+                kernel_size=3,
+                activation='prelu')
 
         x = tf.add(x, x_)
 
         for j in range(2):
-            x = gg.layers.subpixel_convolution(x, upscale_ratio=2, activation='prelu')
+            x = gg.layers.subpixel_conv(
+                x,
+                upscale_ratio=2,
+                activation='prelu')
 
-        x = gg.layers.conv_2d(x, kernel_size=3, filters=3, stride=1, activation='sigmoid')
+        x = gg.layers.conv_2d(
+            x,
+            kernel_size=3,
+            filters=3,
+            stride=1,
+            activation='sigmoid')
 
         return x
 
